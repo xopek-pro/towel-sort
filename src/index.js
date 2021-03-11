16035901a487af61ef0c2b7e3e4738d76346c918
xopek-pro <xopek.pro@internet.ru>
2021-03-12 01:05:56 +0300

    feat: implement the so-called 'towel sort' algorithm.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = 
+function towelSort (matrix) {
+  if (matrix === undefined || matrix.length==0){
+    return [];
+  }
+//  debugger;
+  let result = [];
+  // let direction = 1;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i % 2){
+        result.push(matrix[i][matrix[i].length-j-1]);
+      } else {
+        result.push(matrix[i][j]);
+      }
+    }
+  }
+
+  return result;
 }
+
+// console.log(towelSort([
+//   [1, 2],
+//   [3, 4],
+// ]));
+// shood return [ 1, 2, 4, 3 ]
+
+// console.log(towelSort([
+//       [1, 2, 3],
+//       [4, 5, 6],
+//       [7, 8, 9],
+//     ]));
+// shood return [ 1, 2, 3, 6, 5, 4, 7, 8, 9 ]
